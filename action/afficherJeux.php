c25421772dde4b3414a84698ed02754555297206
imarrainesouhail202@gmail.com <imarrainesouhail202@gmail.com>
2025-01-06 14:01:03 +0100

    delete

--- action/afficherJeux.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once '../config/dataBase.php';
-require_once '../classes/Jeu.php';
-$db = new Database();
-$connex = $db->getConnection();
-session_start();
-$jeu = new Jeu();
-$allgames = $jeu->getAllJeux($connex);
